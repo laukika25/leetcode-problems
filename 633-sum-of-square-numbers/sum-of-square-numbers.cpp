e2a87ea97bbbe1a8f06d68ca0a384397f412e588
Laukika Kulkarni <113303805+laukika25@users.noreply.github.com>
2024-06-17 13:57:31 +0530

    Time: 4 ms (46.70%) | Memory: 7.1 MB (61.35%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        if(c < 0)return false;
+        long left = 0;
+        long right = (int) sqrt(c);
+        while(left <= right){
+            long sum = left*left + right*right;
+            if(sum == c)return true;
+            else if(sum < c)left++;
+            else right--;
+        }
+        return false;
+        
+    }
+};
