fe6b1802237f21cbae606b43396af76c00a4622d
Laukika Kulkarni <113303805+laukika25@users.noreply.github.com>
2024-06-23 13:25:10 +0530

    Time: 3 ms (81.95%) | Memory: 18.3 MB (75.42%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,39 +11,31 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
-        ListNode* dummy = new ListNode(-1);
-        ListNode* x = dummy;
-
         ListNode* t1 = list1;
         ListNode* t2 = list2;
-
-        while(t1!=NULL && t2!=NULL){
-            if(t1->val < t2->val){
+        ListNode* dummy = new ListNode(-1);
+        ListNode* x = dummy;
+        while(t1 != nullptr && t2 != nullptr){
+            if(t1->val <= t2->val){
                 x->next = t1;
-                x = x->next;
                 t1 = t1->next;
-            }
-            else if(t1->val > t2->val){
-                x->next = t2;
                 x = x->next;
-                t2 = t2->next;
             }
             else{
-                x->next = t1;
+                x->next = t2;
+                t2 = t2->next;
                 x = x->next;
-                t1 = t1->next;
             }
         }
-        while(t1){
+        while(t1!=NULL){
             x->next = t1;
-            x = x->next;
             t1 = t1->next;
-        }
-        while(t2){
-            x->next = t2;
             x = x->next;
+        }
+        while(t2!=NULL){
+            x->next = t2;
             t2 = t2->next;
+            x = x->next;
         }
         return dummy->next;
     }
