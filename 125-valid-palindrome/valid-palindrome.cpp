6fec704169c01008fd5f4e7c11e00b989167b051
Laukika Kulkarni <113303805+laukika25@users.noreply.github.com>
2024-06-23 13:34:51 +0530

    Time: 6 ms (45.98%) | Memory: 9 MB (28.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,22 @@
-
 class Solution {
 public:
     bool isPalindrome(string s) {
         string main = "";
-        
-        
-        for(int i = 0; i < s.size(); i++) {
-            if(isalnum(s[i])) {  
-                main.push_back(tolower(s[i])); 
+        for(int i = 0; i<s.size(); i++){
+            if(isalnum(s[i])){
+                main.push_back(tolower(s[i]));
             }
+
         }
-        
         int i = 0;
-        int j = main.size() - 1;
-        while(i < j) {
-            if(main[i] != main[j]) {
-                return false;
+        int j = main.size()-1;
+        while(i < j){
+            if(main[i] != main[j])return false;
+            else{
+                i++;
+                j--;
             }
-            i++;
-            j--;
         }
-        return true; 
+        return true;
     }
 };
