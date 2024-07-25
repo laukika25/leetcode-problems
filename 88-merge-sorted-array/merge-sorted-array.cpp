987480c6e2b93a08749b2599bba6db95e08dce03
Laukika Kulkarni <113303805+laukika25@users.noreply.github.com>
2024-07-25 09:14:40 +0530

    Time: 4 ms (28.90%) | Memory: 11.4 MB (5.92%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
-  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    int i = m - 1; // Start from the end of nums1 (filled elements)
-    int j = n - 1; // Start from the end of nums2
-    int k = m + n - 1; // Index for the merged array (end of nums1)
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>nums;
+        for(int i = m+n-1; i>=m; i--){
+            if(nums1[i] == 0){
+                nums1.pop_back();
+                cout<<"0---"<<endl;
+            }
+            else{
+                cout<<"else---"<<endl;
+                break;
+            }
+        }
+        for(int i = 0; i<nums1.size(); i++){
+            nums.push_back(nums1[i]);
+        }
+        nums1.clear();
+        int i = 0;
+        int j = 0;
+        while(i<nums.size() && j<n){
+            if(nums[i] <= nums2[j]){
+                nums1.push_back(nums[i]);
+                i++;
+            }
+            else{
+                nums1.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i < nums.size()){
+            nums1.push_back(nums[i]);
+            i++;
+        }
+        while(j < nums2.size()){
+            nums1.push_back(nums2[j]);
+            j++;
+        }
 
-    // Loop backwards to fill the merged array from the end
-    while (i >= 0 && j >= 0) {
-      if (nums1[i] > nums2[j]) {
-        nums1[k--] = nums1[i--];
-      } else {
-        nums1[k--] = nums2[j--];
-      }
-    }
 
-    // Copy remaining elements from nums2 (if any)
-    while (j >= 0) {
-      nums1[k--] = nums2[j--];
     }
-  }
 };
