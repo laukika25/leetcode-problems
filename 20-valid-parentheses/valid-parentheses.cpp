3836fbb793585f31586f7ce5f119db954db3d274
Laukika Kulkarni <113303805+laukika25@users.noreply.github.com>
2024-09-15 10:37:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (9.08%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
+        if(s.size() == 1)return false;
+        stack<int>st;
         for(int i = 0; i<s.size(); i++){
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
-                st.push(s[i]);
-            }
+            if(s[i] == '('|| s[i] =='{'||s[i] == '[')st.push(s[i]);
             else{
                 if(!st.empty()){
-                    if(s[i] == '}' && st.top() == '{')st.pop();
-                    else if(s[i] == ']' && st.top() == '[')st.pop();
+                    if(s[i] ==']' && st.top() =='[')st.pop();
+                    else if(s[i] == '}' && st.top() == '{')st.pop();
                     else if(s[i] == ')' && st.top() == '(')st.pop();
                     else return false;
                 }
                 else return false;
+                
             }
+        }
 
-        }
-        if(st.empty()){
-            return true;
-        }
-        return false;
+        if(st.empty())return true;
+        else return false;
     }
 };
